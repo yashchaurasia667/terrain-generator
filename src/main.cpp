6389210d6a2c8158ef8327f090b8dcfb0c101930
yash <yashchaurasia630@gmail.com>
2026-04-13 22:39:49 +0530

    setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,145 +1,211 @@
-#include <renderer.h>
-#include <mesh.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <imgui/imgui.h>
+#include <imgui/backends/imgui_impl_glfw.h>
+#include <imgui/backends/imgui_impl_opengl3.h>
+
+#include <iostream>
 #include <shader.h>
 #include <camera.h>
-#include <utils.h>
 
-#include <vertexArray.h>
-#include <vertexBuffer.h>
-#include <vertexBufferLayout.h>
+unsigned int scr_width = 1280, scr_height = 720;
+bool camera_movement = false;
+Camera camera(glm::vec3(0.0f), 45.0f, 0.1f, 2.5f);
 
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <iostream>
-
-#define STB_IMAGE_IMPLEMENTATION
-
-void gameLoop(GLFWwindow *window, Shader &shader);
-void mouseButtonCallback(GLFWwindow *window, int button, int action, int mods);
-void mouseCallback(GLFWwindow *window, double xposin, double yposin);
-void scrollCallback(GLFWwindow *window, double xoffset, double yoffset);
-void processInput(GLFWwindow *window, int key, int scancodoe, int action, int mods);
 void framebufferSizeCallback(GLFWwindow *window, int width, int height);
-void glfwErrorCallback(int error, const char *description);
+void processInput(GLFWwindow *window);
+void cursorPosCallback(GLFWwindow *window, double xposin, double yposin);
+void mouseButtonCallback(GLFWwindow *window, int button, int action, int mods);
+void scrollCallback(GLFWwindow *window, double xoffset, double yoffset);
 
-bool cameraMovement = false;
-Camera camera(glm::vec3(0.0f, 0.0f, 0.0f), 45.0f, 0.1f, 2.5f);
+float cube_vertices[] = {
+    // position          // texcoords
+    -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+    0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+    -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
 
-Renderer ren("renderer window", 1280, 720, "#version 330 core", true);
+    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+    0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+    -0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
 
-float terrain_vertices[] = {
-    -5.0f, -1.0f, 5.0f,
-    5.0f, -1.0f, 5.0f,
-    5.0f, -1.0f, -5.0f,
+    -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+    -0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+    -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
 
-    -5.0f, -1.0f, 5.0f,
-    5.0f, -1.0f, -5.0f,
-    -5.0f, -1.0f, -5.0f};
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+    0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
 
-VertexArray terrain_vao;
-VertexBuffer terrain_vbo(sizeof(terrain_vertices), terrain_vertices, GL_STATIC_DRAW);
-Shader terrainShader("../shaders/terrain.vs", "../shaders/terrain.fs");
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+    0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+    0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
 
-void createTerrain()
-{
-  VertexBufferLayout terrainLayout;
-  terrainLayout.push<float>(3);
-  terrain_vao.addBuffer(terrain_vbo, terrainLayout);
-}
+    -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+    -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
+    -0.5f, 0.5f, -0.5f, 0.0f, 1.0f};
 
 int main()
 {
-  Renderer::setFrameBufferCallback(framebufferSizeCallback);
-  Renderer::setScrollCallback(scrollCallback);
-  Renderer::setErrorCallback(glfwErrorCallback);
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  GLFWwindow *window = glfwCreateWindow(scr_width, scr_height, "TITLE_HERE", NULL, NULL);
+  if (window == nullptr)
+  {
+    std::cout << "Failed to create a GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to initialize opengl function pointers" << std::endl;
+    return -1;
+  }
+  glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+  glfwSetMouseButtonCallback(window, mouseButtonCallback);
+  glfwSetScrollCallback(window, scrollCallback);
+  glEnable(GL_DEPTH_TEST);
 
-  Renderer::setMouseButtonCallback(mouseButtonCallback);
-  Renderer::setKeyCallback(processInput);
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO io = ImGui::GetIO();
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
 
-  Shader def("../shaders/cube.vs", "../shaders/cube.fs", true, true);
-  createTerrain();
+  ImGui_ImplGlfw_InitForOpenGL(window, false);
+  ImGui_ImplOpenGL3_Init("#version 330 core");
 
-  Renderer::start(gameLoop, def, camera);
+  {
+    Shader shader("../shaders/shader.vs", "../shaders/shader.fs");
 
+    unsigned int vao, vbo;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+
+    glClearColor(0.4, 0.4, 0.4, 0.4);
+    while (!glfwWindowShouldClose(window))
+    {
+      camera.updateFrame();
+
+      glfwPollEvents();
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+      processInput(window);
+
+      ImGui_ImplOpenGL3_NewFrame();
+      ImGui_ImplGlfw_NewFrame();
+      ImGui::NewFrame();
+
+      {
+        ImGui::Begin("Light params");
+        ImGui::End();
+      }
+
+      glm::mat4 model = glm::mat4(1.0f);
+      glm::mat4 view = camera.getViewMatrix();
+      glm::mat4 projection = glm::perspective(camera.getFov(), (float)scr_width / (float)scr_height, 0.1f, 100.0f);
+
+      shader.bind();
+      shader.setMat4("model", model);
+      shader.setMat4("view", view);
+      shader.setMat4("projection", projection);
+
+      glBindVertexArray(vao);
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+
+      ImGui::Render();
+      ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+      glfwSwapBuffers(window);
+    }
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+  }
+
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+  glfwTerminate();
   return 0;
-}
-
-void gameLoop(GLFWwindow *window, Shader &shader)
-{
-  glCall(glClearColor(0.1f, 0.3f, 0.3f, 1.0f));
-  glCall(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
-
-  camera.updateFrame();
-  if (cameraMovement)
-    camera.processMovement(Renderer::getWindow());
-
-  glm::mat4 view = camera.getViewMatrix();
-  glm::mat4 projection = glm::perspective(camera.getFov(), (float)Renderer::width / (float)Renderer::height, 0.1f, 100.0f);
-
-  glm::mat4 terrain_model = glm::mat4(1.0f);
-  glm::vec3 pos = camera.getPos();
-  pos.y = -1.0f;
-  terrain_model = glm::translate(terrain_model, pos);
-
-  terrainShader.bind();
-  terrainShader.setMat4("view", view);
-  terrainShader.setMat4("projection", projection);
-  terrainShader.setMat4("model", terrain_model);
-
-  terrain_vao.bind();
-  glCall(glDrawArrays(GL_TRIANGLES, 0, 6));
-
-  shader.bind();
-  shader.setMat4("view", view);
-  shader.setMat4("projection", projection);
-
-  shader.setFloat("material.shininess", 24.0f);
 }
 
 void framebufferSizeCallback(GLFWwindow *window, int width, int height)
 {
-  Renderer::width = width;
-  Renderer::height = height;
+  scr_width = width;
+  scr_height = height;
   glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow *window, int key, int scancode, int action, int mods)
+void processInput(GLFWwindow *window)
 {
-  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     glfwSetWindowShouldClose(window, true);
+  camera.processMovement(window);
+}
+
+void cursorPosCallback(GLFWwindow *window, double xposin, double yposin)
+{
+  float xpos = static_cast<float>(xposin);
+  float ypos = static_cast<float>(yposin);
+  camera.updateView(xpos, ypos);
 }
 
 void mouseButtonCallback(GLFWwindow *window, int button, int action, int mods)
 {
-  if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+  ImGui_ImplGlfw_MouseButtonCallback(window, button, action, mods);
+  if (ImGui::GetIO().WantCaptureMouse)
+    return;
+
+  if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
   {
-    Renderer::setCursorMode(GLFW_CURSOR_DISABLED);
-    Renderer::setCursorPosCallback(mouseCallback);
-    cameraMovement = true;
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, cursorPosCallback);
+    camera_movement = true;
   }
   else if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_RELEASE)
   {
     camera.firstMouse = true;
-    cameraMovement = false;
-    Renderer::setCursorMode(GLFW_CURSOR_NORMAL);
-    Renderer::setCursorPosCallback(nullptr);
+    camera_movement = false;
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    glfwSetCursorPosCallback(window, nullptr);
   }
-}
-
-void mouseCallback(GLFWwindow *window, double xposin, double yposin)
-{
-  // std::cout << "mouse" << std::endl;
-  float x = static_cast<float>(xposin);
-  float y = static_cast<float>(yposin);
-  camera.updateView(x, y);
 }
 
 void scrollCallback(GLFWwindow *window, double xoffset, double yoffset)
 {
-  camera.updateZoom((float)yoffset);
-}
-
-void glfwErrorCallback(int error, const char *description)
-{
-  std::cout << "ERROR::GLFW::" << description << std::endl;
+  float yoff = static_cast<float>(yoffset);
+  camera.updateZoom(yoff);
 }
