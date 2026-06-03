4a0c25d18f50b2bd2b8a88acd0e5d4616b149fc0
yash <yashchaurasia630@gmail.com>
2026-06-03 11:04:08 +0530

    moved noise shader to terrain

--- src/terrain.cpp
+++ src/terrain.cpp
@@ -14,10 +14,25 @@ Terrain::Terrain(int chunkWidth, int cellWidth, int noiseSeed,
 
 Terrain::~Terrain() {}
 
-void Terrain::initShader(const char *vert, const char *frag,
-                         const char *geometry, const char *tess_control,
+void Terrain::initShader(const char *compute, int rezScale, const char *vert,
+                         const char *frag, const char *geometry,
+                         const char *tess_control,
                          const char *tess_evaluation) {
+  noiseShader = ComputeShader(compute);
   shader = Shader(vert, frag, geometry, tess_control, tess_evaluation);
+
+  unsigned int texResolution = rezScale * chunkWidth;
+  glGenTextures(1, &noise_tex);
+  glBindTexture(GL_TEXTURE_2D, noise_tex);
+
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, texResolution, texResolution, 0,
+               GL_RGBA, GL_FLOAT, nullptr);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  initTerrain(rezScale);
 }
 
 void Terrain::uploadVertexData() {
@@ -29,8 +44,8 @@ void Terrain::uploadVertexData() {
 
 void Terrain::generateVertices() {
   // std::vector<float> vertices;
-  for (unsigned int i = 0; i < rez; i++) {
-    for (unsigned int j = 0; j < rez; j++) {
+  for (int i = 0; i < rez; i++) {
+    for (int j = 0; j < rez; j++) {
       vertices.push_back(-chunkWidth / 2.0f +
                          (chunkWidth * i) / (float)rez); // v.x
       vertices.push_back(0.0f);                          // v.y
@@ -67,8 +82,31 @@ void Terrain::generateVertices() {
   // return vertices;
 }
 
+void Terrain::initTerrain(int rezScale) {
+  unsigned int texResolution = rezScale * chunkWidth;
+  noiseShader.bind();
+  noiseShader.setInt("u_seed", noiseSeed);
+  noiseShader.setVec2("u_chunkOffset", glm::vec2(0.0f));
+  noiseShader.setInt("u_cellWidth", cellWidth);
+  noiseShader.setInt("u_chunkWidth", chunkWidth);
+  noiseShader.setInt("u_noisePass", noisePass);
+  // noiseShader.setFloat("u_amplitude", amp);
+  noiseShader.setFloat("u_frequency", freq);
+  noiseShader.setFloat("u_slopeStrength", slopeStrength);
+  noiseShader.setFloat("u_lacunarity", lacunarity);
+  noiseShader.setFloat("u_persistance", persistance);
+
+  glBindImageTexture(0, noise_tex, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
+  noiseShader.setInt("u_heightMap", 0);
+  glDispatchCompute((texResolution + 15) / 16, (texResolution + 15) / 16, 1);
+  glMemoryBarrier(GL_TEXTURE_FETCH_BARRIER_BIT);
+}
+
 void Terrain::render(Camera camera, glm::mat4 model, glm::mat4 projection,
                      int heightMap) {
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, noise_tex);
+
   shader.bind();
   shader.setMat4("model", model);
   shader.setMat4("view", camera.getViewMatrix());
@@ -79,6 +117,22 @@ void Terrain::render(Camera camera, glm::mat4 model, glm::mat4 projection,
   shader.setInt("MAX_TESS_LEVEL", tess_max_level);
   shader.setFloat("MIN_DISTANCE", tess_min_dist);
   shader.setFloat("MAX_DISTANCE", tess_max_dist);
+
+  shader.setFloat("u_amplitude", amp);
+  shader.setFloat("u_chunkWidth", chunkWidth);
+  shader.setInt("u_noisePass", noisePass);
+
+  shader.setVec3("u_lightDir", glm::normalize(lightDir));
+  shader.setVec3("u_lightColor", glm::normalize(lightColor));
+  shader.setVec3("u_viewPos", camera.getPos());
+
+  shader.setFloat("u_texScale", texScale);
+  shader.setInt("u_normalMap", 1);
+  shader.setVec3("u_terrainColor", terrainColor);
+
+  shader.setVec3("u_snowColor", snowColor);
+  shader.setFloat("u_snowSlopeMax", snowSlopeMax);
+  shader.setFloat("u_snowSlopeMin", snowSlopeMin);
 
   vao.bind();
   glDrawArrays(GL_PATCHES, 0, rez * rez * 4);
--- src/terrain.h
+++ src/terrain.h
@@ -1,36 +1,56 @@
 #pragma once
 
 #include <glad/glad.h>
+
 #include <GLFW/glfw3.h>
-
-#include <shader.h>
-#include <camera.h>
 #include <glm/gtc/matrix_transform.hpp>
+#include <vector>
 
+#include <camera.h>
+#include <shader.h>
 #include <vertexArray.h>
 #include <vertexBuffer.h>
 #include <vertexBufferLayout.h>
 
-#include <vector>
-
 class Terrain {
 public:
-  int chunkWidth, cellWidth, noiseSeed;
-  float tess_min_dist = 10, tess_max_dist = 800;
+  // terrain vars
+  int chunkWidth, cellWidth, rez = 20, drawDist = 2;
   int tess_min_level = 4, tess_max_level = 64;
-  unsigned int rez = 20;
+  float tess_min_dist = 10, tess_max_dist = 800;
+  float texScale = 15.5f, slopeStrength = 1.2f, snowSlopeMax = 0.3f,
+        snowSlopeMin = 1.2f;
+
+  // noise vars
+  int noiseSeed = 5;
+  int noisePass = 10;
+  float amp = 128.0f, freq = 0.29f, persistance = 0.43f, lacunarity = 2.7f;
+
+  // lighting/color vars
+  glm::vec3 lightDir = glm::vec3(0.6f, 1.0f, 0.4f),
+            lightColor = glm::vec3(0.47f, 0.34f, 0.26f),
+            ambient = glm::vec3(0.62f, 0.59f, 1.0f),
+            terrainColor = glm::vec3(0.35, 0.28, 0.15),
+            snowColor = glm::vec3(1.0);
+
+  // shaders
+  ComputeShader noiseShader;
   Shader shader;
+
+  // temp
+  unsigned int noise_tex = 0;
 
   Terrain(int chunkWidth = 1000, int cellWidth = 200, int noiseSeed = 0,
           unsigned int rez = 20);
   ~Terrain();
 
-  void initShader(const char *vert, const char *frag,
-                  const char *geometry = nullptr,
+  void initShader(const char *compute, int rezScale, const char *vert,
+                  const char *frag, const char *geometry = nullptr,
                   const char *tess_control = nullptr,
                   const char *tess_evaluation = nullptr);
   void generateVertices();
   void uploadVertexData();
+  void initTerrain(int rezScale = 1);
   void render(Camera camera, glm::mat4 model, glm::mat4 projection,
               int heightMap);
 
